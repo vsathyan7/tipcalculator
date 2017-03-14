b8691daf84529cf87a4b513c407e05a23f7f3535
Sathya <vsathyan@hotmail.com>
2017-03-13 21:45:46 -0700

    updated project and recording

---
+++ Tipcalc/UIViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExtension.swift
+//  Tipcalc
+//
+//  Created by Sathya Venkataraman on 3/12/17.
+//  Copyright © 2017 Sathya Venkataraman. All rights reserved.
+//
+
+import Foundation
